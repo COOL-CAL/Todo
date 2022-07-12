388e7b80c36de6c52f19cfeb972fb0cb993132bf
COOL_CAL <grin0330@gmail.com>
2022-07-12 12:48:48 +0900

    first commit

---
+++ application/controllers/TodoController.php
@@ -0,0 +1,27 @@
+<?php
+    namespace application\controllers;
+    use application\libs\Application;
+
+    class TodoController extends Controller {
+        public function index() {
+            switch(getMethod()) {
+                case _GET:
+                    return $this->model->selTodoList();
+                
+                case _POST:
+                    $json = getJson();
+                    return [ "result" => $this->model->insTodo($json)];
+
+                case _DELETE:
+                    $urlPaths = getUrlPaths();
+                    $param = [
+                        "itodo" => 0
+                    ];
+
+                    if(isset($urlPaths[2])) {
+                        $param["itodo"] = intval($urlPaths[2]);
+                    }
+                    return [ "result" => $this->model->delTodo($param)];
+            }
+        }
+    }
